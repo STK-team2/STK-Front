1bae61db7482b1bc1d48f98301f75f1a75977fbb
hjy080530 <bomaye119@gmail.com>
2026-03-31 10:44:31 +0900

    refactor: icon을 분리

--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -1,20 +1,9 @@
 /** @jsxImportSource @emotion/react */
 import { useState } from 'react';
 import * as s from './style';
+import { EyeIcon } from '@/shared/ui/Icons';
 
 const stkBg = '/STK.svg';
-
-const EyeIcon = ({ show }: { show: boolean }) =>
-  show ? (
-    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path d="M17.94 17.94A10.07 10.07 0 0112 20c-7 0-11-8-11-8a18.45 18.45 0 015.06-5.94M9.9 4.24A9.12 9.12 0 0112 4c7 0 11 8 11 8a18.5 18.5 0 01-2.16 3.19m-6.72-1.07a3 3 0 11-4.24-4.24M1 1l22 22" stroke="#9497A0" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-    </svg>
-  ) : (
-    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z" stroke="#9497A0" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-      <circle cx="12" cy="12" r="3" stroke="#9497A0" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-    </svg>
-  );
 
 const RegisterPage = () => {
   const [showPassword, setShowPassword] = useState(false);
---
+++ src/shared/ui/Icons/SearchIcon.tsx
@@ -0,0 +1,10 @@
+/** @jsxImportSource @emotion/react */
+
+const SearchIcon = () => (
+  <svg width="18" height="18" viewBox="0 0 24 24" fill="none">
+    <circle cx="11" cy="11" r="8" stroke="#9497A0" strokeWidth="2"/>
+    <path d="M21 21l-4.35-4.35" stroke="#9497A0" strokeWidth="2" strokeLinecap="round"/>
+  </svg>
+);
+
+export default SearchIcon;
